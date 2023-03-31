e41e1a7b9f582113d4c7f23d70c556c1683181e3
KarolKarnas <karol.karnas@gmail.com>
2023-03-31 16:05:00 +0200

    Fetch data on input
    Fetch data of weather from Weatherapi based on input value,
    According to weather in imputed place fetch matching gif form Giphy.com
    Display the gif as body bg image

--- src/modules/App.js
+++ src/modules/App.js
@@ -3,10 +3,17 @@ import Weather from './Weather';
 class App {
 	constructor() {
 		this.weather = new Weather();
-		this.weather.getWeather();
-		this.weather.getGif();
+		this.weather.getWeather('kiev');
+		// this.weather.getGif();
 		// this.weather.renderGif()
+        document.querySelector('#place').addEventListener('input', this.renderWeather.bind(this))
 	}
+
+    renderWeather() {
+       const searchInput = document.querySelector('#place').value;
+       console.log(searchInput);
+       this.weather.getWeather(searchInput)
+    }
 }
 
 export default App;
--- src/modules/Weather.js
+++ src/modules/Weather.js
@@ -1,30 +1,31 @@
 class Weather {
 	constructor() {}
 
-	async getWeather() {
-		const weather = await fetch(
-			'https://api.weatherapi.com/v1/current.json?key=bfe99891e1d843209e475618232903&q=rzeszow'
-		);
-		const weatherData = await weather.json();
-        console.log(weatherData);
-	}
-
-	async getGif() {
+	async getWeather(place) {
 		try {
-			const search = 'christ'; // input.value
-			const img = document.querySelector('#image');
+            const inputPlace = document.querySelector('#place')
+			const searchPlace = 'kiev';
+			const weather = await fetch(
+				`https://api.weatherapi.com/v1/current.json?key=bfe99891e1d843209e475618232903&q=${place}` //input.value
+			);
+			const weatherData = await weather.json();
+			console.log(weatherData);
+            console.log(weatherData.current.condition.text);
+			//--------- GIF ---------
+
+			const searchGif = weatherData.current.condition.text
 			const random50 = Math.floor(Math.random() * 51);
 
 			const gifFetch = await fetch(
-				`https://api.giphy.com/v1/gifs/search?api_key=YG6xZTHIFxmPG2qrwk9CVBiNoL5r1A65&q=${search}`,
+				`https://api.giphy.com/v1/gifs/search?api_key=YG6xZTHIFxmPG2qrwk9CVBiNoL5r1A65&q=${searchGif}`,
 				{ mode: 'cors' }
 			);
 
 			const gifResponse = await gifFetch.json();
-            img.src = gifResponse.data[random50].images.original.url
-
+			console.log(gifResponse.data[random50].images.original.url);
+			document.body.style.backgroundImage = `url(${gifResponse.data[random50].images.original.url})`;
 		} catch (error) {
-            //handle array < 50
+			//handle array < 50
 			console.log(error);
 		}
 	}
